956a4ddb49696eb4563544d1dfb7ca251ad51514
tiago-amado <tiago.amado@nokia.com>
2023-10-26 23:23:52 +0000

    Added Demo Automation portal

--- nginx/enable_anysec.php
+++
@@ -1,4 +0,0 @@
-<?php
-shell_exec('bash ./test2_enable_anysec_peer.gnmic');
-var_dump(http_response_code(200));
-?>
--- nginx/enable_port.php
+++
@@ -1,4 +0,0 @@
-<?php
-shell_exec('bash ./test1_enable_port.gnmic');
-var_dump(http_response_code(200));
-?>
